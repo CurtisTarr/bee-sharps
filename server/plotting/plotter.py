da027e032c5a85ad12adafe02060fdf7fb488f1d
curtistarr <ctarr901@gmail.com>
2019-10-26 16:46:18 +0100

    Adding plot for gender

--- server/plotting/plotter.py
+++ server/plotting/plotter.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_csv(file_path):
@@ -10,4 +11,49 @@ def plot_csv(file_path):
     plt.ylabel('count')
     plt.xlabel('')
 
+    plt.show()
+
+
+def plot_gender(file_path):
+    data_frame = pd.read_csv(file_path)
+
+    males = 0
+    males_interviewed = 0
+    females = 0
+    females_interviewed = 0
+
+    for index, row in data_frame.iterrows():
+        if row['Gender'] == 'M':
+            males += 1
+            if row['Interviewed']:
+                males_interviewed += 1
+        elif row['Gender'] == 'F':
+            females += 1
+            if row['Interviewed']:
+                females_interviewed += 1
+
+    n_groups = 2
+    male_stats = (males, males_interviewed)
+    female_stats = (females, females_interviewed)
+
+    plt.subplots()
+    index = np.arange(n_groups)
+    bar_width = 0.35
+    opacity = 0.8
+
+    plt.bar(index, male_stats, bar_width,
+            alpha=opacity,
+            color='b',
+            label='Male')
+
+    plt.bar(index + bar_width, female_stats, bar_width,
+            alpha=opacity,
+            color='r',
+            label='Female')
+
+    plt.title('Sex vs Interviewed')
+    plt.xticks(index + bar_width, ('Total applicants', 'No. Interviewed'))
+    plt.legend()
+
+    plt.tight_layout()
     plt.show()
