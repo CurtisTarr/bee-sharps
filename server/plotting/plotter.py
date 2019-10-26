7e782018fcd4485322ccc58dd37ba3195eeea7c1
curtistarr <ctarr901@gmail.com>
2019-10-26 18:54:07 +0100

    Making generic plot x vs interviewed method

--- server/plotting/plotter.py
+++ server/plotting/plotter.py
@@ -7,60 +7,58 @@ def plot_csv(file_path):
     data_frame = pd.read_csv(file_path)
     plot_graph(data_frame, 'Age', 'Age Breakdown of Applicants', 'Number of Applicants', 'Age', False)
     plot_graph(data_frame, 'Race', 'Racial Breakdown of Applicants', 'Number of Applicants', 'Race', True)
-    plot_gender(data_frame)
+    plot_vs_interviewed(data_frame, 'Gender')
+    plot_vs_interviewed(data_frame, 'Race')
 
 
-def plot_graph(data_frame, column, title, ylabel, xlabel, shouldUseHist):
-    if shouldUseHist:
+def plot_graph(data_frame, column: str, title: str, y_label: str, x_label: str, should_use_hist: bool):
+    if should_use_hist:
         data_frame[column].hist()
     else:
         data_frame[column].plot()
     plt.title(title)
-    plt.ylabel(ylabel)
-    plt.xlabel(xlabel)
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
 
     plt.show()
 
 
-def plot_gender(data_frame):
-    males = 0
-    males_interviewed = 0
-    females = 0
-    females_interviewed = 0
-
-    for index, row in data_frame.iterrows():
-        if row['Gender'] == 'M':
-            males += 1
-            males_interviewed += row.Interviewed
-        elif row['Gender'] == 'F':
-            females += 1
-            females_interviewed += row.Interviewed
-
-    n_groups = 2
-    male_stats = (males, males_interviewed)
-    female_stats = (females, females_interviewed)
+def plot_vs_interviewed(data_frame, column: str):
+    unique_column_values = data_frame[column].unique()
 
     plt.subplots()
-    index = np.arange(n_groups)
-    bar_width = 0.35
+    index = np.arange(2)
+    bar_width = 0.15
     opacity = 0.8
 
-    plt.bar(index, male_stats, bar_width,
-            alpha=opacity,
-            color='b',
-            label='Male')
+    iteration = 0
+    for value in unique_column_values:
+        if not isinstance(value, str):
+            continue
 
-    plt.bar(index + bar_width, female_stats, bar_width,
-            alpha=opacity,
-            color='r',
-            label='Female')
+        data = get_info_for_row(data_frame, column, value)
+        plt.bar(index + (bar_width * iteration), data, bar_width,
+                alpha=opacity,
+                label=value)
 
-    plt.title('Sex vs Interviewed')
+        iteration += 1
+
+    plt.title(column + ' vs Interviewed')
     plt.xticks(index + bar_width, ('Total applicants', 'No. Interviewed'))
     plt.legend()
-
-    plt.tight_layout()
     plt.show()
+
+
+def get_info_for_row(data_frame, column_label: str, column_value: str) -> (int, int):
+    total = 0
+    interviewed = 0
+
+    for index, row in data_frame.iterrows():
+        if row[column_label] == column_value:
+            total += 1
+            interviewed += row.Interviewed
+
+    return total, interviewed
 
 
 def convert_interview_bool(data_frame):
